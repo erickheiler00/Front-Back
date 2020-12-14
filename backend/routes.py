3cdd79ddcab9f126b9000e7b689b184ff77fffc7
erickheiler00 <erick.jose.heiler@gmail.com>
2020-12-13 21:24:14 -0300

    avanço no frontend

--- backend/routes.py
+++ backend/routes.py
@@ -1,27 +1,32 @@
-from config import app, db, jsonify, request 
+from config import app, db, jsonify, request
 from modelo import Filme, AluguelFilme, Locadora
  
-@app.route("/") 
-def main(): 
-    return 'Sistema de cadastro de filmes.' + '<a href="/listar_filmes"> Operação listar</a>' 
- 
+
+@app.route("/")
+def main():
+    return 'Sistema de cadastro de filmes.' + '<a href="/listar_filmes">Operação listar</a>'
+
+
 @app.route("/listar_filmes") 
 def listar_filmes(): 
-    # obter os filmes do cadastro 
     filmes = db.session.query(Filme).all() 
-    # fornecer a lista de filmes para a página que exibe as pessoas 
     filmes_em_json = [ filme.json() for filme in filmes ]
     resposta = jsonify(filmes_em_json)
     resposta.headers.add("Access-Control-Allow-Origin", "*") 
-    return resposta    
+    return resposta   
 
 @app.route("/incluir_filme", methods=['post'])
 def incluir_filme():
+    resposta = jsonify({"resultado": "ok", "detalhes": "ok"})
     dados = request.get_json()
-    novo_filme = Filme(**dados)
-    db.session.add(novo_filme)
-    db.session.commit()
-    return {"resultado":'ok'}
+    try:
+        novo_filme = Filme(**dados)
+        db.session.add(novo_filme)
+        db.session.commit()
+    except Exception as e:
+        resposta = jsonify({"resultado":"erro", "detalhes":str(e)})
+    resposta.headers.add("Access-Control-Allow-Origin", "*") 
+    return resposta
 
 @app.route("/excluir_filme/<int:filme_id>", methods=['DELETE']) 
 def excluir_filme(filme_id): 
@@ -31,6 +36,14 @@ def excluir_filme(filme_id):
         db.session.commit() 
     except Exception as e: 
         resposta = jsonify({"resultado":"erro", "detalhes":str(e)}) 
+    resposta.headers.add("Access-Control-Allow-Origin", "*") 
+    return resposta 
+
+@app.route("/listar_aluguel_filme")
+def listar_aluguel_filme():
+    aluguel_filme = db.session.query(AluguelFilme).all()
+    lista_jsons = [ x.json() for x in aluguel_filme ]
+    resposta = jsonify(lista_jsons)
     resposta.headers.add("Access-Control-Allow-Origin", "*") 
     return resposta 
 
@@ -48,3 +61,4 @@ def listar(classe):
     resposta = jsonify(lista_jsons)
     resposta.headers.add("Access-Control-Allow-Origin", "*")
     return resposta
+
--- frontend/js/meuscript.js
+++ frontend/js/meuscript.js
@@ -1,27 +1,24 @@
-$( document ).ready(function() {
- 
-    $("#conteudoInicial").removeClass("invisible");
+//$( document ).ready(function() {
 
-    $("#link_listar_filmes").click(function() { 
-    
-    function mostrar_filmes() {
+$(function() {
+    function exibir_filmes() {
         $.ajax({
-            url: 'http://localhost:5000/listar/Filme',
+            url: 'http://localhost:5000/listar_filmes',
             method: 'GET',
             dataType: 'json', 
-            success: listar_filmes, 
-            error: function() {
+            success: listar, 
+            error: function(problema) {
                 alert("erro ao ler dados, verifique o backend");
             }
         });
 
-        function listar_filmes(filmes) {        
+        function listar (filmes) {        
 
             $("#corpoTabelaFilmes").empty();
-            mostrar_conteudo("filmes");  
+            mostrar_conteudo("cadastroFilme");  
 
             for (var i in filmes) {
-                lin = '<tr id="linha_filme_'+filmes[i].id+'">' +
+                lin = '<tr id="linha_'+filmes[i].id+'">' +
                     "<td>" + filmes[i].id + "</td>" +
                     "<td>" + filmes[i].titulo + "</td>" +
                     "<td>" + filmes[i].genero + "</td>" +
@@ -36,143 +33,65 @@ $( document ).ready(function() {
         }
     }; 
 
-        function mostrar_aluguel_filme() {
-            $.ajax({
-                url: 'http://localhost:5000/listar/AluguelFilme',
-                method: 'GET',
-                dataType: 'json',
-                success: listar_aluguel_filme,
-                error: function () {
-                    alert("erro ao ler dados, verifique o backend");
-                }
-            });
-        }    
+    function mostrar_conteudo(identificador) {
+        $("cadastroFilme").addClass("d-none");
+        $("#conteudoInicial").addClass("d-none");
+        $("cadastroAluguelFilme").addClass("d-none");
+        $("#"+identificador).removeClass("d-none");
 
+    }
 
-        function listar_aluguel_filme (aluguel_filme) {
-            $('#corpoTabelaAluguelFilme').empty();
+    $(document).on("click", "#linkListarFilmes", function() {
+        exibir_filmes();
+    });
 
-            mostrar_conteudo("aluguel_filme");
+    $(document).on("click", "#linkInicio", function() {
+        mostrar_conteudo("conteudoInicial");
+    });
 
-                for (var i in aluguel_filme) {
-                    lin = '<tr id="linha_aluguel_filme_'+aluguel_filme[i].id+'">)'+
-                        '<td>' + aluguel_filme[i].data_aluguel + '</td>' +
-                        '<td>' + aluguel_filme[i].data_devolucao + '</td>' +
-                        '<td>' + aluguel_filme[i].preco + '</td>' +
-                        '<td>' + aluguel_filme[i].filme_id + '</td>' +
-                        '<td>' + aluguel_filme[i].filme + '</td>' 
-                        '<td><a href=# id="excluir_' + aluguel_filme[i].id + '"' +
-                        'class="excluir_aluguel_filme"><img src="imagens/excluir.jpg"' +
-                        'alt="Excluir aluguel filme" title="Excluir aluguel filme" height="30"></a>' +
-                        '</td>' +
-                        "</tr>";
-                    $('#corpoTabelaAluguelFilme').append(lin);
-                }
-        };
+    $(document).on("click", "btIncluirFilme", function() {
+        id = $("#campoId").val();
+        titulo = $("campoTitulo").val();
+        genero = $("campoGenero").val();
+        ano = $("campoAno").val();
 
-        function mostrar_locadora() {
-            $.ajax({
-                url: 'http://localhost:5000/listar/Locadora',
-                method: 'GET',
-                dataType: 'json',
-                success: listar_locadora,
-                error: function () {
-                    alert("erro ao ler dados, verifique o backend");
-                }
-            })
-        }
-
-
-        function listar_locadora(locadora) {
-            $('#corpoTabelaLocadora').empty();
-
-            mostrar_conteudo("locadora");
-
-            for (var i in locadora) {
-                lin = '<tr id="linha_locadora_'+locadora[i].id+'">)'+
-                    '<td>' + locadora[i].nome + '</td>' +
-                    '<td>' + locadora[i].endereco + '</td>' +
-                    '<td>' + locadora[i].telefone + '</td>' +
-                    '<td>' + locadora[i].filme_id + '</td>' +
-                    '<td>' + locadora[i].filme_mais_vendido + '</td>' 
-                    '<td>' + locadora[i].filme.id + '</td>' 
-                    '<td>' + locadora[i].filme.titulo + '</td>' 
-                    '<td>' + locadora[i].filme.genero + '</td>' 
-                    '<td>' + locadora[i].filme.ano + '</td>' 
-                    '<td><a href=# id="excluir_' + locadora[i].id + '"' +
-                    'class="excluir_locadora"><img src="imagens/excluir.jpg"' +
-                    'alt="Excluir locadora" title="Excluir locadora" height="30"></a>' +
-                    '</td>' +
-                    "</tr>";
-                $('#corpoTabelaLocadora').append(lin);
-            }
-        }
-
-        function mostrar_conteudo(identificador) {
-            $("filmes").addClass("d-none");
-            $("aluguel_filme").addClass("d-none");
-            $("locadora").addClass("d-none");
-            $("#conteudoInicial").addClass("d-none");
-            $('#${identificador}').removeClass("d-none");
-        }
-
-
-        $(document).on("click", "#mostrar_filmes", () => { 
-            mostrar_filmes();
-        });
-
-        $(document).on("click", "#mostrar_aluguel_filme", () => { 
-            mostrar_aluguel_filme();
-        });
-
-        $(document).on("click", "#mostrar_locadora", () => { 
-            mostrar_locadora();
-        });
-
-
-        $("#btn_incluir_filme").click(function() { 
-
-        id = $("#id").val();
-        titulo = $("#titulo").val();
-        genero = $("#genero").val();
-        ano = $("#ano").val();
-
-        dados = JSON.stringify({id: id, titulo: titulo, 
-            genero: genero, ano: ano});
+        var dados = JSON.stringify({ id: id, titulo: titulo, genero: genero, ano: ano });
 
         $.ajax({
-            url : 'http://localhost:5000/incluir_filme',
-            type : 'POST',
-            contentType: 'application/json',
+            url: 'http://localhost:5000/incluir_filme',
+            method: 'POST',
             dataType: 'json',
+            contentType: 'application/json',
             data: dados,
-            success: incluirFilme,
-            error: erroIncluirFilme
+            success: filmeIncluido,
+            error: erroAoIncluir
         });
-        function incluirFilme(resposta) {
-            if (resposta.resultado == "ok") {
-                alert('Filme incluído com sucesso');
 
-                $("#id_filme").val("");
-                $("#titulo").val("");
-                $("#genero").val("");
-                $("#ano").val("");
+        function filmeIncluido (retorno) {
+            if (retorno.resultado == "ok") {
+                alert("Filme incluído com sucesso!")
+
+                $("#campoId").val("");
+                $("#campoTitulo").val("");
+                $("#campoGenero").val("");
+                $("#campoAno").val("");
             } else {
-                alert('erro na comunicação');
+                alert(retorno.resultado + ":" + retorno.detalhes);
             }
         }
-        function erroIncluirFilme(resposta) {
-            alert("Algo de errado não está certo na chamada ao back-end");
-        }
 
+        function erroAoIncluir (retorno) {
+            alert("erro ao incluir dados, verifique o backend: ");
+        }
     });
 
     $('#modalIncluirFilme').on('hide.bs.modal', function (e) {
-        if (! $("#tabelaFilmes").hasClass('d-none')) {
-           mostrar_filmes();
+        if (! $("#cadastroFilme").hasClass('d-none')) {
+           exibir_filmes();
        }
-   });
+    });
 
+    mostrar_conteudo("conteudoInicial");
 
     $(document).on("click", ".excluir_filme", function() {
         var componente_clicado = $(this).attr('id');
@@ -200,8 +119,52 @@ $( document ).ready(function() {
         function erroAoExcluir (retorno) {
             alert("erro ao excluir dados, verifique o backend: ");
         }
+    }); 
+    
+
+    function exibir_alugueis_realizados() {
+        $.ajax({
+            url: 'http://localhost:5000/listar_aluguel_filme',
+            method: 'GET',
+            dataType: 'json',
+            success: listar,
+            error: function(problema) {
+                alert("erro ao ler dados, verifique o backend");
+            }
+        });
+    
+
+
+        function listar (alugueis_realizados) {
+            $('#corpoTabelaAluguelFilme').empty();
+
+            mostrar_conteudo("cadastroAluguelFilme");
+
+                for (var i in alugueis_realizados) {
+                    lin = '<tr id="linha_aluguel_realizado'+alugueis_realizados[i].id+'">)'+
+                        '<td>' + alugueis_realizados[i].data_aluguel + '</td>' +
+                        '<td>' + alugueis_realizados[i].data_devolucao + '</td>' +
+                        '<td>' + alugueis_realizados[i].preco + '</td>' +
+                        '<td>' + alugueis_realizados[i].filme.id + '</td>' +
+                        '<td>' + alugueis_realizados[i].filme.titulo + '</td>' +
+                        '<td>' + alugueis_realizados[i].filme.genero + '</td>' +
+                        '<td>' + alugueis_realizados[i].filme.ano + '</td>' +
+                        '<td><a href=# id="excluir_aluguel_realizado' + alugueis_realizados[i].id + '"' +
+                        'class="excluir_aluguel_realizado"><img src="imagens/excluir.jpg"' +
+                        'alt="Excluir aluguel filme" title="Excluir aluguel filme" height="30"></a>' +
+                        '</td>' +
+                        "</tr>";
+                    $('#corpoTabelaAluguelFilme').append(lin);
+                }
+        }
+
+    }
+
+    $(document).on("click", "#linkListarAluguelFilmeRealizado", function() {
+        exibir_alugueis_realizados();
     });
-
-    mostrar_resultado("conteudoInicial")
-
 });
+
+
+
+
